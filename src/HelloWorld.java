cecb2b201d9ed6988b8c32664a439c3ea62a2e8e
ivanbozhkovadm <ivanbozhkovadm@outlook.com>
2015-10-04 14:17:25 +0300

    FirstLesson

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class HelloWorld{
+	
+	public static void main(String[] args){
+		int integerOne = 22;
+		int integerTwo = 55;
+		int integerThree = integerOne + 1;
+		int integerFour = integerOne + integerTwo;
+		boolean trueOrFalse = true;
+		trueOrFalse = false;
+		char randomChar = 'a';
+		char anotherChar = 65;
+		String randomString = "I am random STRING.";
+		String anotherString = "And it is cool :)";
+		String andAnotherString = randomString + " " + anotherString;
+		System.out.println(integerFour);
+		System.out.println(Double.MAX_VALUE); 
+		System.out.println(randomChar);
+		System.out.println(integerThree);
+		System.out.println(trueOrFalse);
+		System.out.println(randomChar);
+		System.out.println(anotherChar);
+		System.out.println(randomString);
+		System.out.println(andAnotherString);
+	}
+}
